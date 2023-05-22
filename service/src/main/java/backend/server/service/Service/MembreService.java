31246948f3e3e9a852dc510cf571bd80efdd7c9b
HAJIB Diae <110051837+i-wavebyte@users.noreply.github.com>
2023-05-22 16:49:11 +0100

    update
    Added pagable user feature up to the service layer

--- service/src/main/java/backend/server/service/Service/MembreService.java
+++ service/src/main/java/backend/server/service/Service/MembreService.java
@@ -1,12 +1,17 @@
 package backend.server.service.Service;
 
+import backend.server.service.POJO.PageResponse;
 import backend.server.service.Repository.MembreRepository;
 import backend.server.service.domain.Membre;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 @Transactional @Slf4j
@@ -33,6 +38,34 @@ public class MembreService {
 
     public Membre getMembre(String username){
         return membreRepository.findByUsername(username);
+    }
+
+    public PageResponse<Membre> getMembresPage(int page, int size, String sortBy, String searchQuery, String sortOrder, String groupFilter ){
+
+        Sort.Direction direction = Sort.Direction.fromString(sortOrder);
+        Sort sort = Sort.by(direction, sortBy);
+        int start = page * size;
+        int end = Math.min(start + size, (int) membreRepository.count());
+        List<Membre> membres = membreRepository.findAll(sort);
+
+        if (searchQuery != null && !searchQuery.isEmpty()){
+            membres = membres.stream()
+                    .filter(membre -> membre.getUsername().toLowerCase().contains(searchQuery.toLowerCase())
+                            || membre.getNom().toLowerCase().contains(searchQuery.toLowerCase())
+                            || membre.getPrenom().toLowerCase().contains(searchQuery.toLowerCase())
+                            || membre.getEmail().toLowerCase().contains(searchQuery.toLowerCase())
+                    )
+                    .collect(Collectors.toList());
+        }
+
+        if (groupFilter != null && !groupFilter.isEmpty()) {
+            membres = membres.stream()
+                    .filter(professor -> professor.getGroupe().getNom().equalsIgnoreCase(groupFilter))
+                    .collect(Collectors.toList());
+        }
+
+        List<Membre> pageContent = membres.subList(start, Math.min(end, membres.size()));
+        return new PageResponse<>(pageContent, membres.size());
     }
 
 }
